bd79789d3a0fb11ca9624dc9bdd904b47aaa2498
Alex Kozlov <oleksandr.kozlov@lyfo.com>
2024-11-11 03:17:13 +0100

    Code for testing purpose. Will be removed on release.

--- app/src/main/java/com/darkliself/engenioustask/MainActivity.kt
+++ app/src/main/java/com/darkliself/engenioustask/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.darkliself.engenioustask
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
+import com.darkliself.engenioustask.ui.screens.mainscreen.MainScreenViewModel
 import com.darkliself.engenioustask.ui.theme.EngeniousTaskTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -34,10 +43,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    val viewModel = hiltViewModel<MainScreenViewModel>()
+    val users = viewModel.usersData.collectAsLazyPagingItems()
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+
+        LazyColumn() {
+            items(users.itemCount) {
+                Log.e("ItemCount", "${users.itemCount}")
+                Text(text = users[it]!!.login)
+                Spacer(modifier = Modifier.height(10.dp))
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
