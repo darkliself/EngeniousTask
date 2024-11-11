153411d865ded8686d1c2d99d428789d46bfa83a
Alex Kozlov <oleksandr.kozlov@lyfo.com>
2024-11-12 00:43:31 +0100

    Added ConnectionStateUseCase.kt

---
+++ app/src/main/java/com/darkliself/engenioustask/model/ConnectionStateUseCase.kt
@@ -0,0 +1,13 @@
+package com.darkliself.engenioustask.model
+
+import com.darkliself.engenioustask.data.connectivity.ConnectivityDataSource
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ConnectionStateUseCase @Inject constructor(
+    private val connectivityDataSource: ConnectivityDataSource,
+) {
+    operator fun invoke(): Flow<Boolean> {
+        return connectivityDataSource.isActive
+    }
+}
