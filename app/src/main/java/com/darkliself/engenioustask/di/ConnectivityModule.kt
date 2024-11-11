cf77a54ddbdeb5dad2533e0a463ae73c397f665a
Alex Kozlov <oleksandr.kozlov@lyfo.com>
2024-11-12 00:42:19 +0100

    Added ConnectivityModule.kt

---
+++ app/src/main/java/com/darkliself/engenioustask/di/ConnectivityModule.kt
@@ -0,0 +1,28 @@
+package com.darkliself.engenioustask.di
+
+import android.content.Context
+import android.net.ConnectivityManager
+import com.darkliself.engenioustask.data.connectivity.ConnectivityDataSource
+import com.darkliself.engenioustask.data.connectivity.ConnectivityManagerDataSource
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+@Suppress("kotlin:S6517")
+interface ConnectivityModule {
+    @Binds
+    fun bindConnectivityManagerDataSource(connectivityManagerDataSource: ConnectivityManagerDataSource): ConnectivityDataSource
+    companion object {
+        @Provides
+        @Singleton
+        fun provideConnectivityManager(@ApplicationContext context: Context): ConnectivityManager =
+            context.getSystemService(ConnectivityManager::class.java)
+
+    }
+}
