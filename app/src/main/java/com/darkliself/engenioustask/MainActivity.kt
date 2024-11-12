129e50d30ec01747a67265aa9649ba05bd579ef3
Alex Kozlov <oleksandr.kozlov@lyfo.com>
2024-11-12 16:23:06 +0100

    Cleaned MainActivity.kt from unused stuff.

--- app/src/main/java/com/darkliself/engenioustask/MainActivity.kt
+++ app/src/main/java/com/darkliself/engenioustask/MainActivity.kt
@@ -1,28 +1,17 @@
 package com.darkliself.engenioustask
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.paging.compose.collectAsLazyPagingItems
 import com.darkliself.engenioustask.data.connectivity.ConnectivityManagerDataSource
 import com.darkliself.engenioustask.ui.screens.mainscreen.MainScreen
-import com.darkliself.engenioustask.ui.screens.mainscreen.MainScreenTestViewModel
 import com.darkliself.engenioustask.ui.theme.EngeniousTaskTheme
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -37,7 +26,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EngeniousTaskTheme {
-                Scaffold(modifier = Modifier.fillMaxSize().padding(top = 30.dp)) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     MainScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -47,31 +36,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val viewModel = hiltViewModel<MainScreenTestViewModel>()
-    val users = viewModel.userPagingData.collectAsLazyPagingItems()
-
-    Column(modifier = Modifier.fillMaxSize().zIndex(1f)) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier
-        )
-
-        LazyColumn() {
-            items(users.itemCount) {
-                Log.e("ItemCount", "${users.itemCount}")
-                Text(text = users[it]!!.login)
-                Spacer(modifier = Modifier.height(10.dp))
-            }
-        }
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     EngeniousTaskTheme {
-        Greeting("Android")
+        MainScreen(modifier = Modifier)
     }
 }
