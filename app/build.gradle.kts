6899140f2353a2a93eaf54a79fbb6b99323aaccf
Alex Kozlov <oleksandr.kozlov@lyfo.com>
2024-11-12 17:38:00 +0100

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,7 @@ dependencies {
     implementation (libs.androidx.paging.runtime)
     implementation(libs.androidx.paging.compose)
     implementation(libs.coil.compose)
-    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.2")
+    implementation(libs.coil.network.okhttp)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
