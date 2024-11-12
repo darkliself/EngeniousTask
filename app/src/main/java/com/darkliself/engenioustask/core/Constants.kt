600df388e61abde4b3282c361b035d9cf684418d
Alex Kozlov <oleksandr.kozlov@lyfo.com>
2024-11-12 16:17:38 +0100

    Added constants classes and variables to avoid magic number etc.

---
+++ app/src/main/java/com/darkliself/engenioustask/core/Constants.kt
@@ -0,0 +1,12 @@
+package com.darkliself.engenioustask.core
+
+import androidx.compose.ui.unit.dp
+
+object Constants {
+    val MEDIUM_SPACER_HEIGHT = 16.dp
+    val USER_CARD_HEIGHT = 400.dp
+    val CORNER_RADIUS_MEDIUM = 16.dp
+    val CARD_SPACER = 12.dp
+    val PADDING_DEFAULT = 16.dp
+    val CORNER_RADIUS_LARGE = 32.dp
+}
---
+++ app/src/main/java/com/darkliself/engenioustask/core/TextConstants.kt
@@ -0,0 +1,6 @@
+package com.darkliself.engenioustask.core
+
+object TextConstants {
+    val SEARCH_DISABLE = "Search will not work"
+    val USER_CARD_CONTENT_DESCRIPTION = "Avatar of "
+}
